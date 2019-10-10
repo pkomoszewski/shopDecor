1d0fb9e807561f11833dc32cb8e0e73546d7f4a6
Paweł Komoszewski <pkomoszewski@gmail.com>
2019-10-10 22:29:18 +0200

    create product detals screen

---
+++ lib/screens/product_detals_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class ProductDetalsScreen extends StatelessWidget {
+  const ProductDetalsScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("cos")),
+      body: Column(),
+    );
+  }
+}
