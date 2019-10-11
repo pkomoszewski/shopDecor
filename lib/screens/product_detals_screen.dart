89e309b6982860aa86005d1e9197d31ca5676ba7
Paweł Komoszewski <pkomoszewski@gmail.com>
2019-10-12 00:42:50 +0200

    modify screen details product

--- lib/screens/product_detals_screen.dart
+++ lib/screens/product_detals_screen.dart
@@ -9,21 +9,30 @@ class ProductDetalsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final id =ModalRoute.of(context).settings.arguments as String;
-final loadedProduct =Provider.of<Products>(context).findById(id);
+    final id = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context).findById(id);
     return Scaffold(
-      appBar: AppBar(title: Text(loadedProduct.title)),
-      body: Column(children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.money_off),
-          title: Text(loadedProduct.title),
-          subtitle: Text(loadedProduct.description),
-          trailing: Text(loadedProduct.price.toString()),
-        )
-      ],
-       
-
-      
-    ));
+        appBar: AppBar(title: Text(loadedProduct.title)),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width,
+              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover),
+            ),
+            Expanded(
+                child: Container(
+                    color: Colors.black12,
+                    child: ListTile(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                      leading: Icon(Icons.money_off),
+                      title: Text(loadedProduct.title),
+                      subtitle: Text(loadedProduct.description),
+                      trailing:Text('${loadedProduct.price} PLN', style:TextStyle(fontSize: 20, color: Theme.of(context).primaryColor))
+                      
+                    )))
+          ],
+        ));
   }
 }
