e3a1aa7cfeb52233081cc119b26499f8878c3f83
Pawel Komoszewski <pkomoszewski@gmail.com>
2019-10-17 18:23:51 +0200

    add gradient

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -18,14 +18,33 @@ class FavoriteScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text("Favorite"),
         ),
-        body: ListView.builder(
-            itemCount: favoriteProducts.length,
-            itemBuilder: (ctx, i) => ListTileProduct(
-                  favoriteProducts[i].id,
-                  favoriteProducts[i].title,
-                  favoriteProducts[i].description,
-                  favoriteProducts[i].price,
-                  favoriteProducts[i].toogleFavorite,
-                )));
+        body: Container(
+            // Add box decoration
+            decoration: BoxDecoration(
+              // Box decoration takes a gradient
+              gradient: LinearGradient(
+                // Where the linear gradient begins and ends
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                // Add one stop for each color. Stops should increase from 0 to 1
+                stops: [0.1, 0.6, 0.7, 0.9],
+                colors: [
+                  // Colors are easy thanks to Flutter's Colors class.
+                  Colors.indigo[800],
+                  Colors.indigo[700],
+                  Colors.indigo[600],
+                  Colors.indigo[400],
+                ],
+              ),
+            ),
+            child: ListView.builder(
+                itemCount: favoriteProducts.length,
+                itemBuilder: (ctx, i) => ListTileProduct(
+                      favoriteProducts[i].id,
+                      favoriteProducts[i].title,
+                      favoriteProducts[i].description,
+                      favoriteProducts[i].price,
+                      favoriteProducts[i].toogleFavorite,
+                    ))));
   }
 }
--- lib/widgets/listTileProduct.dart
+++ lib/widgets/listTileProduct.dart
@@ -13,31 +13,40 @@ class ListTileProduct extends StatelessWidget {
     return Dismissible(
         key: ValueKey(id),
         background: Container(
-          color: Theme.of(context).errorColor,
-          child: Icon(
-            Icons.delete,
-            color: Colors.white,
-            size: 40,
-          ),
-          alignment: Alignment.centerRight,
-          padding: EdgeInsets.only(right: 20),
-          margin: EdgeInsets.symmetric(
-            horizontal: 15,
-            vertical: 4,
+          child: Card(
+            color: Theme.of(context).errorColor,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  "Delete",
+                  style: TextStyle(
+                      color: Theme.of(context).accentColor, fontSize: 24),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ],
+            ),
           ),
         ),
         direction: DismissDirection.endToStart,
         onDismissed: (direction) => toogleFavorite(),
-        child: ListTile(
-            contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-            leading: IconButton(
-              icon: Icon(Icons.favorite, color: Colors.white),
-              onPressed: () {},
-            ),
-            title: Text(title),
-            subtitle: Text(description),
-            trailing: Text('${price.toString()} PLN',
-                style: TextStyle(
-                    fontSize: 20, color: Theme.of(context).primaryColor))));
+        child: Card(
+          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: ListTile(
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              title: Text(title),
+              subtitle: Text(description),
+              trailing: Text('${price.toString()} PLN',
+                  style: TextStyle(
+                      fontSize: 20, color: Theme.of(context).primaryColor))),
+        ));
   }
 }
